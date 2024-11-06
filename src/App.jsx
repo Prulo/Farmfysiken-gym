a583d300e09d6e09ff8362ae9e4f8853a94f3868
Pontus Segervall <pontus.segervall@hotmail.se>
2024-11-06 21:19:49 +0100

    yo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-yellow-700 flex items-center justify-center m-12 text-center text-3xl">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae nobis
+          excepturi adipisci earum magnam quo aut sed aliquam minus hic delectus
+          itaque nemo numquam est eligendi, quibusdam, eum pariatur. Quod?
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default App;
